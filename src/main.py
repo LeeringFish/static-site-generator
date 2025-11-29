066371b4bf757b1dc1bd5e9c33192229053fc40b
LeeringFish <theleeringfish@proton.me>
2025-11-28 19:53:25 -0500

    add textnode.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from textnode import TextNode
+from textnode import TextType
+
+def main():
+    node = TextNode("Some text", TextType.TEXT)
+    print(node)
+
+
+if __name__ == "__main__":
+    main()

0b2a650e846e72d74f4d40a218ba16b739d93fcb
LeeringFish <theleeringfish@proton.me>
2025-12-01 20:23:19 -0500

    add LeafNode class to htmlnode.py and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_all_none(self):
@@ -18,5 +19,13 @@ class TestHTMLNode(unittest.TestCase):
         props = {"href": "https://www.google.com", "target": "_blank",}
         node = HTMLNode(props=props)
         self.assertEqual(node.props_to_html(), ' href="https://www.google.com" target="_blank"')
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
 
     

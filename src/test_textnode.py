8f54359e14bb31ca02563891739aafef12e7194c
LeeringFish <theleeringfish@proton.me>
2025-12-08 13:47:58 -0500

    add text_to_textnodes function to textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,7 +2,7 @@ import unittest
 
 from textnode import TextNode, TextType 
 from textnode import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links
-from textnode import split_nodes_image, split_nodes_link
+from textnode import split_nodes_image, split_nodes_link, text_to_textnodes
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -166,6 +166,50 @@ class TestTextNode(unittest.TestCase):
             ],
             new_nodes,
     )
+        
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+
+        self.assertListEqual(
+            text_to_textnodes(text),
+
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+        )
+
+    def test_text_to_textnodes_mixed_all_features(self):
+        text = (
+            "Start **bold** then _italic_ and `code` with "
+            "![alt](https://example.com/img.png) plus "
+            "a [link](https://boot.dev) end"
+        )
+
+        expected = [
+            TextNode("Start ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" then ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode(" with ", TextType.TEXT),
+            TextNode("alt", TextType.IMAGE, "https://example.com/img.png"),
+            TextNode(" plus a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+            TextNode(" end", TextType.TEXT),
+        ]
+
+        result = text_to_textnodes(text)
+        self.assertEqual(result, expected)
 
 
 

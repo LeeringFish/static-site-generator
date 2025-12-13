728d7599b7e2b9f4f114f0458521671ab05b0cd5
LeeringFish <theleeringfish@proton.me>
2025-12-12 20:39:57 -0500

    add generate_page function to gencontent.py and hook it into main

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,5 +1,32 @@
+import os
+
+from block_markdown import markdown_to_html_node
+
 def extract_title(markdown):
     for line in markdown.split("\n"):
         if line.startswith("# "):
             return line[2:].strip()
     raise ValueError("no h1 header")
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, "r") as from_file:
+        markdown = from_file.read()
+
+    with open(template_path, "r") as template_file:
+        template = template_file.read()
+
+    dest_dir = os.path.dirname(dest_path)
+    os.makedirs(dest_dir, exist_ok=True)
+
+    html = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    with open(dest_path, "w") as dest_file:
+        dest_file.write(template)
+
+    
--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
-
+import os
 from copystatic import copy_from_source_to_destination
+from gencontent import generate_page
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
-    copy_from_source_to_destination("static", "public")
+    copy_from_source_to_destination(dir_path_static, dir_path_public)
+
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
 
 if __name__ == "__main__":
